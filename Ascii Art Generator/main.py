9b2ce25042cdc441f810bbe06a4d36e613dff2a1
Kartik Krishna <114423746+kartik761@users.noreply.github.com>
2025-10-24 15:03:22 +0530

    Enhance ASCII art generation with new font and output
    Updated character dimensions and added text rendering.

--- Ascii Art Generator/main.py
+++ Ascii Art Generator/main.py
@@ -1,16 +1,16 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 import math
 
-char = "#Wo- "[::-1]
+char = "Wo- "[::-1]
 charArray = list(char)
 charLen = len(charArray)
 interval = charLen/256
 
 ScaleFactor = 0.3
 
-charWidth = 7
-charHeight = 10
+charWidth = 8
+charHeight = 18
 
 def getChar(inputInt):
     return charArray[math.floor(inputInt * interval)]
@@ -18,12 +18,19 @@ def getChar(inputInt):
 text_file = open('output.txt', 'w') 
 
 im = Image.open('Wall-E.jpg')
+
+fnt = ImageFont.truetype("lucon.ttf", 15)
+
 width, height = im.size
 
-im = im.resize((int(ScaleFactor * width), int(ScaleFactor * height)), Image.NEAREST)
+im = im.resize((int(ScaleFactor * width), int(ScaleFactor * height * 0.55)), Image.NEAREST)
 width, height = im.size
 
 pix = im.load()
+
+outputImage = Image.new('RGB', (charWidth * width, charHeight * height), color=(0,0,0))
+d = ImageDraw.Draw(outputImage)
+
 
 
 for i in range(height):
@@ -33,7 +40,8 @@ for i in range(height):
         gray = int(r/3 + g/3 + b/3)
         pix[j,i] = (gray, gray, gray)
         text_file.write(getChar(gray))
+        d.text((j*charWidth, i* charHeight),getChar(gray), font = fnt, fill = (r,g,b))
     
     text_file.write('\n')
 
-im.save('output.png')
+outputImage.save('output.png')
